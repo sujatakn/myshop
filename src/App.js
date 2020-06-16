04173dee5b31e1c3693a74e193246d3fc84780f0
unknown <nsujjik@gmail.com>
2020-06-16 10:52:09 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome</h1>
+     <Header></Header>
+    </div>
+  );
+}
+
+export default App;
